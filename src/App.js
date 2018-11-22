71abf74615a87f6d2abf34d3a9d4e40ea757b8fd
Hong Jeesoo <jeesoo.hong@gmail.com>
2018-11-22 19:38:01 +0900

    Loging을 위한 Modal 컴포넌트(Portal 적용) 추가

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import PostList from "./components/PostList";
 import PostDetail from "./components/PostDetail";
 import NewPostForm from "./components/NewPostForm";
 import EditPostForm from "./components/EditPostForm";
+
+import Modal from "./components/Modal";
+
 import { UserProvider } from "./contexts/UserContext";
 
 // 로그인 폼에 회원가입 버튼 만들기
@@ -24,7 +27,8 @@ class App extends Component {
     this.state = {
       page: "post-list",
       // 현재 보고 있는 게시물의 ID
-      postId: null
+      postId: null,
+      showModal: false
     };
   }
   handleRegisterPage() {
@@ -55,20 +59,26 @@ class App extends Component {
     });
   }
   handleLoginFormPage() {
-    this.setState({
-      page: "login"
-    });
+    this.setState(prevState => ({
+      page: "login",
+      showModal: !prevState.showModal
+    }));
   }
   render() {
     const { page } = this.state;
+    const modal = this.state.showModal;
     return (
       <UserProvider
         onPostListPage={() => this.handlePostListPage()}
         onLoginFormPage={() => this.handleLoginFormPage()}
       >
         <div className="App">
-          {page === "login" ? (
-            <LoginForm onRegister={() => this.handleRegisterPage()} />
+          {page === "login" && modal ? (
+            <Modal>
+              <div className="modal">
+                <LoginForm onRegister={() => this.handleRegisterPage()} />
+              </div>
+            </Modal>
           ) : page === "register" ? (
             <RegisterForm onPostList={() => this.handlePostListPage()} />
           ) : page === "post-list" ? (
